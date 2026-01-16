ad392d7f3d6d89de234a93b3c36f6a9f19ae3a0f
Ollé Séraphin ! <olleseraphin6@gmail.com>
2026-01-16 09:22:00 +0100

    Complete Menu

--- TPE_Algo_G10.c
+++ TPE_Algo_G10.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 #include<math.h>
-#include<string.h>
 
-typedef struct {
-
-    char matricule[25];
+typedef struct
+{
+  char matricule[25];
     char nom[30];
     char prenom[30];
     char date_naiss[15];
@@ -12,63 +12,123 @@ typedef struct {
     char departement[30];
     char filiere[30];
     char region_origine[30];
-    int lines;
-											}G10;
-    G10 s1[];
-	int main() {
-    int i;
-    int lignes;
-    int option;
-printf("MENU \n");
-      printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n", "1. Enregistrer", "2. Modifier", "3. Recherher (matricule)" , "4. Supprimer", "5. Trier (ordre Alphabetique)", "6. Rechercher (Dichotomie)", "7. Calculateur d'option", "8. Trier par Filiere", "9. Afficher la liste des etudiants");
-			printf("\n");
-      printf("Choisir une option [1-9]: \n");
-        scanf("%d",&option);
-    
-		while ( option < 1 || option >9 )
-    {
-					printf("\n");
-          printf("ERREUR !!! Choisir un nombre valide [1-9] \n");
-					printf("MENU \n");
-          printf("%s \n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n", "1. Enregistrer", "2. Modifier","3. Recherher (matricule)",  "4. Supprimer", "5. Trier (ordre Alphabetique)", "6. Rechercher (Dichotomie)", "7. Calculateur d'age", "8. Trier par Filiere", "9. Afficher la liste des etudiants");
-					printf("\n");
-					scanf("%d",&option);
-		}
-if (option==1){
-	printf("combien d'etudiants voulez vous enregistrer ? \n");
-        scanf("%d",&lignes);
-			G10 s1[lignes];
-    for (i = 0; i < lignes; i++)
-    {
-      printf("Entrer le matricule de l'etudiant %d: \n", i+1);
-        scanf("%s",s1[i].matricule);
-      printf("Entrer le nom de l'etudiant %d: \n", i+1);
-        scanf(" %30[^\n]",s1[i].nom);
-      printf("Entrer le prenom de l'etudiant %d: \n", i+1);
-        scanf(" %30[^\n]",s1[i].prenom);
-      printf("Entrer la date de naissance de l'etudiant %d (JJ/MM/AAAA): \n", i+1);
-        scanf("%s",s1[i].date_naiss);
-      printf("Entrer le sexe de l'etudiant %d (M/F): \n", i+1);
-        scanf("%s",s1[i].sexe);
-      printf("Entrer le departement de l'etudiant %d: \n", i+1);
-        scanf("%s",s1[i].departement);
-      printf("Entrer la filiere de l'etudiant %d: \n", i+1);
-        scanf("%s",s1[i].filiere);
-      printf("Entrer la region d'origine de l'etudiant %d: \n", i+1);
-        scanf("%s",s1[i].region_origine);
-      printf("\n");
-    }
+} G10;
+G10* Gestion; // Revoir aussi les pointeurs !
 
-printf("\n Liste des etudiants inscrits: \n");
+  int j=0;
+  int lignes=0;
+  int i;
+  int confirmation;
+
+void enregistrer(){
+      printf("\n Entrer le matricule de l'etudiant %d: \n", i+1);
+        scanf("%s",Gestion[i].matricule);
+      printf("Entrer le nom de l'etudiant %d: \n", i+1);
+        scanf(" %30[^\n]",Gestion[i].nom);
+      printf("Entrer le prenom de l'etudiant %d: \n", i+1);
+        scanf(" %30[^\n]",Gestion[i].prenom);
+      printf("Entrer la date de naissance de l'etudiant %d (JJ/MM/AAAA): \n", i+1);
+        scanf("%s",Gestion[i].date_naiss);
+      printf("Entrer le sexe de l'etudiant %d (M/F): \n", i+1);
+        scanf("%s",Gestion[i].sexe);
+      printf("Entrer le departement de l'etudiant %d: \n", i+1);
+        scanf("%s",Gestion[i].departement);
+      printf("Entrer la filiere de l'etudiant %d: \n", i+1);
+        scanf("%s",Gestion[i].filiere);
+      printf("Entrer la region d'origine de l'etudiant %d: \n", i+1);
+        scanf("%s",Gestion[i].region_origine);
+      printf("\n");
+    j++;
+      printf("Etudiant enregistre avec succes !\n");
+}
+void afficher(){
+		if (j==0)
+		{
+      printf("AUCUN ETUDIANT ENREGISTRE ! \n");
+			printf("\n");
+      return;
+		}
+      printf("\n Liste des etudiants inscrits: \n");
       printf("%-05s %-15s %-15s %-15s %-15s %-15s %-15s %-15s %-15s \n", "Num.", "Matricule",  "Noms",  "Prenoms",  "date_Naiss.",  "Sexe",  "Departement",  "Filiere",  "Region D'origine");
 			printf("\n");
-    for ( i = 0; i < lignes; i++)
-    {
-      printf("%-05d %-15s %-15s %-15s %-15s %-15s %-15s %-15s %-15s\n", i+1, s1[i].matricule, s1[i].nom, s1[i].prenom, s1[i].date_naiss, s1[i].sexe, s1[i].departement, s1[i].filiere, s1[i].region_origine);
-    }
+		for ( i = 0; i < j; i++)
+		{
+      printf("%-05d %-15s %-15s %-15s %-15s %-15s %-15s %-15s %-15s\n", i+1, Gestion[i].matricule, Gestion[i].nom, Gestion[i].prenom, Gestion[i].date_naiss, Gestion[i].sexe, Gestion[i].departement, Gestion[i].filiere, Gestion[i].region_origine);
 
-} 
-		
-
-  return 0;
+			//printf("\n");
+		}
 }
+  int main(){
+
+  int choix;
+	do {
+continuation:
+		printf("\n MENU DE GESTION \n");
+		printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n", "1. Enregistrer", "2. Modifier", "3. Recherher (matricule)" , "4. Supprimer", "5. Trier (ordre Alphabetique)", "6. Rechercher (Dichotomie)", "7. Calculateur d'option", "8. Trier par Filiere", "9. Afficher la liste des etudiants");
+		//printf("\n");
+		printf("0. Quitter le programme \n");
+      printf("Choisir une option [0-9]: \n");
+		printf("Entrez votre choix \n");
+			scanf("%d", &choix);
+		printf("\n");
+	switch (choix)
+	{
+	case 1:
+				printf("Combien d'etudiants voulez vous enregistrer ?: ");
+					scanf("%d", &lignes);
+					Gestion=(G10*)malloc(lignes * sizeof(G10)); // Comprendre davantage l'allocation dynamique !
+							if (Gestion==0) {
+				printf("Vous ne pouvez pas enregistrer 0 etudiants !");
+							return 1; 
+															}
+							for ( i = 0; i < lignes; i++){
+									enregistrer();
+																						}
+					break;
+	case 9:
+					afficher();
+					break;
+		case 2:
+					printf("Hello 2");
+					break;
+		case 3:
+					printf("Hello 3");
+					break;
+		case 4:
+					printf("Hello 4");
+		case 5:
+					printf("Hello 5");
+					break;
+		case 6:
+					printf("Hello 6");
+					break;
+		case 7:
+					printf("Hello 7");
+					break;
+		case 8:
+					printf("Hello 8");
+					break;
+		case 0:
+          printf("Voulez vous vraiment sortir de l'application?\n  1. Oui  2. Non \n");
+          scanf("%d",&confirmation);
+				if (confirmation==1)
+				{
+					
+					printf("Au revoir !");
+					break;
+				} else {
+goto continuation;
+								}
+				
+		default:
+			if (choix>=10)
+			{
+					printf(" \n ERREUR ! choisir entre [0-9] \n");
+					printf("\n");
+			}
+						break;
+	} 
+	}while(choix!=0);
+  
+  return 0; 
+  }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,5 @@
+void ajouterEtudiants();
+void afficherEtudiants();
+void modifierEtudiant();
+void supprimerEtudiant();
+void trierAlphabetiquement();
